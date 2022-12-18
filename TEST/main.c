0f299ad8b5a97ed216f4df4af84751074e2aa412
bluesky5837645 <bluesky5837645@gmail.com>
2022-12-18 14:59:57 +0800

    first commit

---
+++ TEST/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("Hello World!\n");
+    return 0;
+}
+
+/*
+除了要安裝cmake之外,也要安裝build-essential
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get install -y build-essential
+*/
